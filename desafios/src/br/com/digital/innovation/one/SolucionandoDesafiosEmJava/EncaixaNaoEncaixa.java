697eeffdd9f43910981f3644e363435de503bc25
Samuel Barbosa Almeida <barbosasamuel24@gmail.com>
2021-08-24 21:20:50 -0300

    Desafios feitos

---
+++ desafios/src/br/com/digital/innovation/one/SolucionandoDesafiosEmJava/EncaixaNaoEncaixa.java
@@ -0,0 +1,23 @@
+package br.com.digital.innovation.one.SolucionandoDesafiosEmJava;
+
+import java.util.Scanner;
+
+public class EncaixaNaoEncaixa {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        String a, b;
+        int N = leitor.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            a = leitor.next();
+            b = leitor.next();
+
+            if (a.endsWith(b))
+                System.out.println("encaixa");
+            else
+                System.out.println("nao encaixa");
+        }
+
+        leitor.close();
+    }
+}
