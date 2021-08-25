6a13fdb5a0e8b68ba37da3e44d7861a0f9ab8c22
Samuel Barbosa Almeida <barbosasamuel24@gmail.com>
2021-08-24 21:22:18 -0300

    Erro de código no desafio de média corrigido

--- desafios/src/br/com/digital/innovation/one/SolucionandoDesafiosEmJava/Medias.java
+++ desafios/src/br/com/digital/innovation/one/SolucionandoDesafiosEmJava/Medias.java
@@ -1,42 +1,54 @@
 package br.com.digital.innovation.one.SolucionandoDesafiosEmJava;
 
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Scanner;
 
-public class Medias {
+public class Desafio{
+
     public static void main(String[] args)  throws IOException {
         Scanner sc = new Scanner(System.in);
+        // sc.useLocale(Locale.ENGLISH);
+        // Locale.setDefault(new Locale("en", "US"));
 
-        float n1, n2, n3, n4, media, emexame, exameemedia;
+        double n1,n2,n3,n4,media,emexame,emexamefinal;
 
-        n1 = sc.nextFloat();
-        n2 = sc.nextFloat();
-        n3 = sc.nextFloat();
-        n4 = sc.nextFloat();
+        n1 = sc.nextDouble();
+        n2 = sc.nextDouble();
+        n3 = sc.nextDouble();
+        n4 = sc.nextDouble();
 
-        media = (n1*2 + n2*3 + n3*4 + n4) / 10;
-        System.out.printf("Media %.1f\n", media);
+        media = (n1*2 + n2*3 + n3*4 + n4)/10 ;
 
-        if (media > 7.0) {
-            System.out.println("Aluno aprovado.");
-        } else if(media > 6.9 && media < 7.0) {
-            // unspecified case
-        } else if (media >= 5.0) {
-            System.out.println("Aluno em exame.");
 
-            emexame = sc.nextFloat();
+        System.out.printf("Media: %.1f\n", media);
+
+
+        if (media >= 7.0){
+            System.out.printf("Aluno aprovado.\n");
+        }
+        else if (media < 5.0){
+            System.out.printf("Aluno reprovado.\n");
+        }
+        else if (media <= 6.9){
+            System.out.printf("Aluno em exame.\n");
+
+            emexame = sc.nextDouble();
+            emexamefinal = (emexame + media)/2.0;
+
             System.out.printf("Nota do exame: %.1f\n", emexame);
 
-            exameemedia = (media + emexame) / 2;
+            if( emexamefinal >= 5.0){
+                System.out.printf("Aluno aprovado.\n");
+                System.out.printf("Media final: %.1f\n", emexamefinal);
+            }
+            else{
+                System.out.printf("Aluno reprovado\n");
+                System.out.printf("Media final: %.1f\n", emexamefinal);
 
-            if (exameemedia >= 5.0)
-                System.out.println("Aluno aprovado.");
-            else
-                System.out.println("Aluno reprovado");
-
-            System.out.printf("Media final: %.1f\n", exameemedia);
-        } else {
-            System.out.println("Aluno reprovado.");
+            }
         }
+
+        sc.close();
     }
 }
